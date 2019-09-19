239c417332f8f7892cc0f4e8bec79b52a1cd32b8
Julia Ver <jverchyonova@gmail.com>
2019-09-20 00:40:52 +0300

    Architecture & main routes fixed

--- index.js
+++ index.js
@@ -1,37 +1,27 @@
 // index.js
 // главный файл
 ;
-const express = require('express');
-const MongoClient = require('mongodb').MongoClient;
-const dbConfig = require ('./config/db');
+const express        = require('express');
+const bodyParser     = require('body-parser');
+const MongoClient    = require('mongodb').MongoClient;
+
+// смотри пример конфига здесь: config/dbTemplate.js
+const dbUrl          = require ('./config/db').url;
+const dbName         = require ('./config/db').database;
 
 const app = express();
 const port = 3000;
 
-const client = new MongoClient (dbConfig.url,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true }
-);
+const client = new MongoClient (dbUrl, {useNewUrlParser: true, useUnifiedTopology: true });
 
-client.connect ((err, database) => {
-// client.connect ((err, connection) => {
-    // if (err) throw err;
+client.connect ((err, cluster) => {
+    if (err) throw err;
     if (err) return console.log(err);
 
-    require('./app/routes') (app, database);
-    // require('./app/routes/') (
-    //     app,
-    //     connection.db(dbConfig.database)
-    // );
+    require('./app/routes') (app, cluster.db(dbName));
 
     app.listen(port, () => {
         console.log('We are live on ', port);
     });
 });
 
-// ниже коммент для меня (jv),
-//нужно разобраться и подумать, как лучше обращаться к коллекциям - здесь общей переменной (стр.21) или каждый у себя к своей коллекции уточняет путь, а в мейн - к обеим коллекциям, а здесь в переменную записать только базу, а не коллекции....
-// Еще:  возможно, проще в одну коллекцию запихнуть оба типа заметок....:
-// client.connect ((err, database) => {
-//     require('./app/routes') (app, database);

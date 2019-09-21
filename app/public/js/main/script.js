f9caf621f421fefaa95216734f13de53aaf0d430
Julia Ver <jverchyonova@gmail.com>
2019-09-21 22:05:56 +0300

    views cleaned - header & main page

--- app/public/js/main/script.js
+++ app/public/js/main/script.js
@@ -1,30 +1,30 @@
 // app/public/js/script.js
-// В СВЕТЕ ТОГО, ЧТО СТРОИМ СТРАНИЦУ ПОЛНОСТЬЮ С СЕРВЕРА, НУЖНО ПЕРЕСМОТРЕТЬ СТРУКТУРУ КЛИЕНТСКИХ JSов
 ;
-/** MAIN CONTROLLER */
-// файл, управляющий работой других js файлов. Не должен иметь прямого доступа и обращения к названиям и содержанию DOM-элементов или к серверу.
-//Обращение к DOM-элементам - через переменные и методы из UICtrl.
-//Обращение к серверу - через вызов методов из RqstCtrl.
 
-((UICtrl, RqstCtrl) => {
+// let DOM = {
+//     $body: $('body'),
+//     $headerLogo: $('header-logo'),
+//     $noteButton: $('#note-button'),
+//     $listButton: $('#list-button'),
+// };
 
-    let DOM = UICtrl.getDOM(); // получение элементов из HTML через переменные от UICtrl
+// let hrefs = {
+//     mainHref: location.href,
+//     notesHref: `${location.href}notes`,
+//     apiNotesHref: `${location.href}api/notes/`,
+//     listsHref: `${location.href}lists`,
+//     apiListsHref: `${location.href}api/lists/`
+// }
 
-    const composeResult = UICtrl.composeResult; //метод создания HTML-элементов (из UICtrl)
+// DOM.$headerLogo.on('click', function () {
+//     location.href=hrefs.mainHref;
+// })
 
-    const requestImitation = RqstCtrl.requestImitation; //метод получения данных с сервера
+// DOM.$noteButton.on('click', function () {
+//     location.href=hrefs.notesHref;
+// });
 
+// DOM.$listButton.on('click', function () {
+//     location.href=hrefs.listsHref;
+// });
 
-    DOM.$testButton.on('click', function (event) {
-        event.preventDefault();
-        try {
-            $(this).next()[0]
-                ? $(this).next().remove()
-                : $(this).after(composeResult(requestImitation()));
-        } catch (err) {
-            console.log('ERROR HERE: ', err);
-        }
-    });
-
-
-})(UserInterfaceController, RequestController);

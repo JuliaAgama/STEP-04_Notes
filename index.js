1121aeedeacc8777f911a727c966ef1c030ddf1f
JuliaAgama <jverchyonova@gmail.com>
2019-09-22 10:37:42 +0300

    views cleaned - header & main page (#3)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // главный файл
 ;
 const express        = require('express');
-const bodyParser     = require('body-parser');
+// const bodyParser     = require('body-parser');
 const MongoClient    = require('mongodb').MongoClient;
 
 // смотри пример конфига здесь: config/dbTemplate.js
@@ -15,7 +15,7 @@ const port = 3000;
 const client = new MongoClient (dbUrl, {useNewUrlParser: true, useUnifiedTopology: true });
 
 client.connect ((err, cluster) => {
-    if (err) throw err;
+    // if (err) throw err;
     if (err) return console.log(err);
 
     require('./app/routes') (app, cluster.db(dbName));

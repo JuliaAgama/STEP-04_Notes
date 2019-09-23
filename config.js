898ffe1412f76977c22fabbb9d177ac912c4228b
Julia Ver <jverchyonova@gmail.com>
2019-09-23 18:42:42 +0300

    architecture changes for HEROKU deployment

--- config.js
+++ config.js
@@ -8,7 +8,7 @@ const root       = path.join.bind(this, __dirname);
 dotenv.config({ path: root('.env') });
 
 module.exports = {
-    PORT: process.env.PORT || 3000,
+    PORT: process.env.PORT == null || process.env.PORT =="" ? 5000 : process.env.PORT,
     MONGODB_URL: process.env.MONGODB_URL,
     MONGODB_DBNAME: process.env.MONGODB_DBNAME
 }

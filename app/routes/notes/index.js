5601bccf3b28e7f4e6265b60afa8436fcf9c1853
molenHS <molenathome@gmail.com>
2019-09-24 20:09:46 +0300

    NOTES FORM GET AND REDIRECT AND DB POST (#9)
    * NOTES FORM GET AND REDIRECT AND DB POST
    
    * Deleted db.js из коммита Саши Ш.
    
    Этот файл больше не используется

--- app/routes/notes/index.js
+++ app/routes/notes/index.js
@@ -3,24 +3,67 @@
 // - карточка с названием (опционально) и текстом
 ;
 
+// app/routes/main/index.js
+// маршруты к главной странице "/"
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const assert = require('assert');
+const path = require('path');
+const mongodb = require('mongodb');
+
+// const router = express.Router();
+// const ObjectID = require('mongodb').ObjectID;
 
 
+module.exports.notes = function (app, database) { //
 
-module.exports.notes = function (db){ //
+    app.use(bodyParser.urlencoded({extended: false}));
+    app.use(express.static('app/public'));
+
+    app.set('view engine', 'pug');
+    app.set('views', './app/views');
+
+    app.route('/notes')
+        .get((req, res) => {
+            res.render('notes')
+        });
 
     /**
-    * Роут GET /notes, который будет отдавать HTML страницу с формой создания заметки.
-    * Роут GET /notes/${id}, который будет отдавать HTML страницу детального отображения заметки.
-    */
+     * Роут GET /notes, который будет отдавать HTML страницу с формой создания заметки.
+     * Роут GET /notes/${id}, который будет отдавать HTML страницу детального отображения заметки.
+     */
 
 };
 
-module.exports.api_notes = function (db) {
+// module.exports.api_notes = function (db) {
+//
+//     /**
+//      * Роут POST /api/notes для создания заметки.
+//      * Роут PUT /api/notes/${id} для редактирования заметки.
+//      * Роут DELETE /api/notes/${id} для удаления заметки.
+//      */
+//
+// };
 
-    /**
-    * Роут POST /api/notes для создания заметки.
-    * Роут PUT /api/notes/${id} для редактирования заметки.
-    * Роут DELETE /api/notes/${id} для удаления заметки.
-    */
+module.exports.api_notes = function (app, db) {
 
+    app.use(bodyParser.json());
+    app.post('/api/notes', async function (req, res) {
+        
+        try {
+            let insertedNote = await db.collection('notes').insertOne(req.body);
+            assert.equal(1, insertedNote.insertedCount);
+            res.status(201);
+            // res.send('Note created');
+            res.redirect('/')
+            // res.redirect('/')
+        }
+        
+        catch (err) {
+            console.log(err);
+        }
+        
+       
+    });
 };
